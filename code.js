a0139493deb6d08372e983af49e0ff08fd6b32e2
Kevin Skayro <kev1n-1997@outlook.com>
2020-10-02 11:38:47 -0400

    fixed some css and added selectors on js file

---
+++ code.js
@@ -0,0 +1,8 @@
+//Selectors
+const burgerMenu = document.querySelector(".burger_menu");
+const userInput = document.querySelector(".user_input");
+const today = document.querySelector(".today");
+const tomorrow = document.querySelector(".tomorrow");
+const threeDays = document.querySelector(".three_days");
+//Event listeners
+//Functions
