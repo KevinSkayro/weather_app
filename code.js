6699ccaf3a134d23616c54688f709177604c2744
Kevin Skayro <kev1n-1997@outlook.com>
2020-10-09 10:04:05 -0400

    added all data parameters and a catch to the data fetch to let user know when an input is not valid

--- code.js
+++ code.js
@@ -3,12 +3,16 @@ const api = {
   base: "http://api.weatherapi.com/v1",
 };
 //Selectors
+let date = document.querySelector(".date");
+let time = document.querySelector(".time");
 const burgerMenu = document.querySelector(".burger_menu");
 const userInput = document.querySelector(".user_input");
 const searchBtn = document.querySelector(".search_btn");
+const selectDay = document.querySelectorAll(".day_selection");
 const today = document.querySelector(".today");
 const tomorrow = document.querySelector(".tomorrow");
 const threeDays = document.querySelector(".three_days");
+const maxNminTemp = document.querySelector(".highLow");
 const actualTemp = document.querySelector(".actual_temp");
 const feelsLikeTemp = document.querySelector(".feels_like_temp");
 const weatherCondition = document.querySelector(".weather_state");
@@ -28,17 +32,41 @@ function getQuery(e) {
 }
 
 function updateSearch(query) {
-  fetch(`${api.base}/current.json?key=${api.key}&q=${query}`)
+  fetch(`${api.base}/forecast.json?key=${api.key}&q=${query}&days=3`)
     .then((weather) => {
       return weather.json();
     })
-    .then(displayWeather);
+    .then(displayWeather)
+    .catch(() => {
+      date.innerText = "";
+      time.innerText = "";
+      maxNminTemp.innerHTML = "";
+      actualTemp.innerHTML = "Not Found";
+      feelsLikeTemp.innerHTML = "Try again with a different input";
+      weatherCondition.innerHTML = "";
+      wind.innerHTML = "0";
+      rain.innerHTML = "0";
+      humidity.innerHTML = "0";
+      windBar.style.width = wind.innerText + "px";
+      rainBar.style.width = wind.innerText + "px";
+      humidityBar.style.width = humidity.innerText + "px";
+    });
 }
 
 function displayWeather(weather) {
   console.log(weather);
-  actualTemp.innerHTML = `${weather.current.temp_f}<span>&deg;F</span>`;
-  feelsLikeTemp.innerHTML = `Feels like ${weather.current.feelslike_f}<span>&deg;F</span>`;
+  maxNminTemp.innerHTML = `Day ${Math.round(
+    weather.forecast.forecastday[0].day.maxtemp_f
+  )}<span>&deg;F</span>/Night ${Math.round(
+    weather.forecast.forecastday[0].day.mintemp_f
+  )}<span>&deg;F</span>`;
+  userInput.value = `${weather.location.name}, ${weather.location.region}`;
+  actualTemp.innerHTML = `${Math.round(
+    weather.current.temp_f
+  )}<span>&deg;F</span>`;
+  feelsLikeTemp.innerHTML = `Feels like ${Math.round(
+    weather.current.feelslike_f
+  )}<span>&deg;F</span>`;
   weatherCondition.innerHTML = `${weather.current.condition.text}`;
   wind.innerHTML = `${weather.current.wind_mph}`;
   windBar.style.width = wind.innerText + "px";
@@ -46,4 +74,46 @@ function displayWeather(weather) {
   rainBar.style.width = wind.innerText + "px";
   humidity.innerHTML = `${weather.current.humidity}`;
   humidityBar.style.width = humidity.innerText + "px";
+
+  let now = new Date();
+
+  date.innerText = dateBuilder(now);
+  if (now.getMinutes() <= 9) {
+    time.innerText = ` ${now.getHours()}:0${now.getMinutes()}`;
+  } else {
+    time.innerText = ` ${now.getHours()}:${now.getMinutes()}`;
+  }
 }
+
+function dateBuilder(dt) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "Dicember",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dt.getDay()];
+  let date = dt.getDate();
+  let month = months[dt.getMonth()];
+  let year = dt.getFullYear();
+
+  return `${month} ${day} ${date} ${year},`;
+}
+// function daySelection() {}
